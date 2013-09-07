256ae01a31aa12718dd7a54c352441b964cd0faf
Francis Papineau <francispapineau@gmail.com>
2013-09-06 21:54:40 -0400

    Trying to fix the USB Sender issues
    I got the RF < - > Arduino working (USB),
    Now i need to get the Arduino < - > PC working (USB)

---
+++ Arduino ADK USB Host/Arduino_ADK_USB_HOST/AVR_USB_Host/Release/USB_HID_API/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../USB_HID_API/oddebug.c \
+../USB_HID_API/usbdrv.c 
+
+S_UPPER_SRCS += \
+../USB_HID_API/usbdrvasm.S 
+
+ASM_SRCS += \
+../USB_HID_API/usbdrvasm.asm 
+
+OBJS += \
+./USB_HID_API/oddebug.o \
+./USB_HID_API/usbdrv.o \
+./USB_HID_API/usbdrvasm.o 
+
+C_DEPS += \
+./USB_HID_API/oddebug.d \
+./USB_HID_API/usbdrv.d 
+
+S_UPPER_DEPS += \
+./USB_HID_API/usbdrvasm.d 
+
+ASM_DEPS += \
+./USB_HID_API/usbdrvasm.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+USB_HID_API/%.o: ../USB_HID_API/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -I"/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino" -I"/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/variants/mega" -I"/Volumes/DataHDD/Documents/4th Year Project/Black-Box-Mouse-Interface/Arduino ADK USB Host/Arduino_ADK_USB_HOST/AVR_USB_Host" -I"/Volumes/DataHDD/Documents/4th Year Project/Black-Box-Mouse-Interface/Arduino ADK USB Host/libraries/EEPROM" -D__IN_ECLIPSE__=1 -DARDUINO=100 -DUSB_PID= -DUSB_VID= -Wall -Os -g -mmcu=atmega2560 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+USB_HID_API/%.o: ../USB_HID_API/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Assembler'
+	avr-gcc -x assembler-with-cpp -mmcu=atmega2560 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+USB_HID_API/%.o: ../USB_HID_API/%.asm
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Assembler'
+	avr-gcc -x assembler-with-cpp -mmcu=atmega2560 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Arduino ADK USB Host/Arduino_ADK_USB_HOST/AVR_USB_Host/Release/USB_Implementation/USB_Functions.d
@@ -0,0 +1,50 @@
+USB_Implementation/USB_Functions.d USB_Implementation/USB_Functions.o:  \
+ ../USB_Implementation/USB_Functions.cpp \
+  ../USB_Implementation/USB_Functions.h \
+  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Arduino.h \
+  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/binary.h \
+  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WCharacter.h \
+  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WString.h \
+  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/HardwareSerial.h \
+  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Stream.h \
+  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Print.h \
+  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Printable.h \
+  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.h \
+  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/variants/mega/pins_arduino.h \
+  ../USB_Implementation/../USB_HID_API/usbconfig.h \
+  ../USB_Implementation/../USB_HID_API/usbdrv.h \
+  ../USB_Implementation/../USB_HID_API/usbconfig.h \
+  ../USB_Implementation/../USB_HID_API/usbportability.h \
+  ../USB_Implementation/../System_Defines/Hardware_Defines.h
+
+../USB_Implementation/USB_Functions.h:
+
+/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Arduino.h:
+
+/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/binary.h:
+
+/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WCharacter.h:
+
+/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WString.h:
+
+/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/HardwareSerial.h:
+
+/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Stream.h:
+
+/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Print.h:
+
+/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Printable.h:
+
+/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.h:
+
+/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/variants/mega/pins_arduino.h:
+
+../USB_Implementation/../USB_HID_API/usbconfig.h:
+
+../USB_Implementation/../USB_HID_API/usbdrv.h:
+
+../USB_Implementation/../USB_HID_API/usbconfig.h:
+
+../USB_Implementation/../USB_HID_API/usbportability.h:
+
+../USB_Implementation/../System_Defines/Hardware_Defines.h:
