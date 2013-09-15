95b9e69d7105c75bf7d3f99c06a73bcd30f1ec4e
Francis Papineau <francispapineau@gmail.com>
2013-09-15 14:02:34 -0400

    Changed the layout of the repo…
    It is now easier to navigate the repo

--- Arduino ADK USB Host/AVR_USB_Host/Release/USB_HID_API/oddebug.d
+++
@@ -1,6 +0,0 @@
-USB_HID_API/oddebug.d USB_HID_API/oddebug.o: ../USB_HID_API/oddebug.c \
-  ../USB_HID_API/oddebug.h ../USB_HID_API/usbportability.h
-
-../USB_HID_API/oddebug.h:
-
-../USB_HID_API/usbportability.h:
