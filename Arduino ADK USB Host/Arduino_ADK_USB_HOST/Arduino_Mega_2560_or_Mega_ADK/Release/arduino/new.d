30b7c31ffe7191a784197eecebd92d91e9328466
Francis Papineau <francispapineau@gmail.com>
2013-09-19 23:40:46 -0400

    deleted unecessary files.

--- Arduino ADK USB Host/Arduino_ADK_USB_HOST/Arduino_Mega_2560_or_Mega_ADK/Release/arduino/new.d
+++
@@ -1,5 +0,0 @@
-arduino/new.d arduino/new.o:  \
- /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.cpp \
-  /Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.h
-
-/Applications/Development/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.h:
